74663481e02b1aec004217a53e56f57b8a6d6ed7
Robert Kuykendall <robert@rkuykendall.com>
2012-07-11 15:09:25 -0400

    Switch Home to a fragment.

--- src/edu/uconn/pha/HomeActivity.java
+++ src/edu/uconn/pha/HomeActivity.java
@@ -1,87 +1,18 @@
 package edu.uconn.pha;
 
-import org.json.JSONException;
-
-import edu.uconn.model.DemographicInfo;
-import edu.uconn.model.Height;
-import edu.uconn.model.Person;
-import edu.uconn.model.Weight;
-import edu.uconn.serverclient.ServerConnection;
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-public class HomeActivity extends Activity {
-
+public class HomeActivity extends FragmentActivity {
 	private static final String TAG = HomeActivity.class.getName();
-
-	private DemographicInfo basicDemographicInfo;
-	private Height height;
-	private ImageView imageView;
-	private Person person;
-	private String fullName;
-	private TextView textView;
-	private Weight weight;
-
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_activity);
 		setTitle(R.string.home);
-
-		// set the HealthVault info header text
-		textView = (TextView) findViewById(R.id.home_activity_healthvault_header);
-		textView.setText(R.string.my_healthvault_info);
-
-		// retrieve person data
-		try {
-			person = ServerConnection.getPersonInfo();
-			basicDemographicInfo = person.getBasicInfo();
-			height = person.getHeight();
-			weight = person.getWeight();
-		} catch (JSONException e) {
-			Log.e(TAG, "Error :: JSON Exception", e);
-		}
-
-		// set and display the user's profile image (generic by default)
-		imageView = (ImageView) findViewById(R.id.home_activity_user_pic);
-		if(basicDemographicInfo.getGender().equalsIgnoreCase("male")) {
-			imageView.setBackgroundResource(R.drawable.profile_male);
-		} else if(basicDemographicInfo.getGender().equalsIgnoreCase("female")) {
-			imageView.setBackgroundResource(R.drawable.profile_female);
-		} else {
-			imageView.setBackgroundResource(R.drawable.profile_unknown);
-		}
-
-		// set and display the user's full name
-		textView = (TextView) findViewById(R.id.home_activity_user_name);
-		fullName = person.getFirstName() + " " + person.getLastName();	
-		textView.setText(fullName);
-
-		// set and display the user's birthdate
-		textView = (TextView) findViewById(R.id.home_activity_user_birthdate);			
-		textView.setText(person.getBirthDate());
-
-		// set and display the user's gender
-		textView = (TextView) findViewById(R.id.home_activity_user_gender);
-		textView.setText(basicDemographicInfo.getGender());
-
-		// set and display the user's race
-		textView = (TextView) findViewById(R.id.home_activity_user_race);		
-		textView.setText(person.getRace());
-
-		// set and display the user's blood type
-		textView = (TextView) findViewById(R.id.home_activity_user_blood_type);			
-		textView.setText(person.getBloodType());
-
-		// set and display the user's height
-		textView = (TextView) findViewById(R.id.home_activity_user_height);		
-		textView.setText(height.getHeightValue() + " inches");
-
-		// set and display the user's weight
-		textView = (TextView) findViewById(R.id.home_activity_user_weight);
-		textView.setText(weight.getLbsValue() + " lbs");
+		
+		Log.v(TAG, "Started Home Activity.");
 	}
 }
--- src/edu/uconn/pha/HomeFragment.java
+++ src/edu/uconn/pha/HomeFragment.java
@@ -1,8 +1,10 @@
 package edu.uconn.pha;
 
-import android.content.Intent;
+import org.json.JSONException;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import edu.uconn.model.DemographicInfo;
 import edu.uconn.model.Height;
 import edu.uconn.model.Person;
 import edu.uconn.model.Weight;
+import edu.uconn.serverclient.ServerConnection;
 
 public class HomeFragment extends Fragment {
 	private static final String TAG = HomeActivity.class.getName();
@@ -27,12 +30,58 @@ public class HomeFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	        Bundle savedInstanceState) {
-	    Intent launchingIntent = getActivity().getIntent();
-	    // String content = launchingIntent.getData().toString();
 	 
 	    View view = inflater.inflate(R.layout.home_fragment, container, false);
-//	    viewer.loadUrl(content);
-//	 
+
+		// retrieve person data
+		try {
+			person = ServerConnection.getPersonInfo();
+			basicDemographicInfo = person.getBasicInfo();
+			height = person.getHeight();
+			weight = person.getWeight();
+		} catch (JSONException e) {
+			Log.e(TAG, "Error :: JSON Exception", e);
+		}
+
+		// set and display the user's profile image (generic by default)
+		imageView = (ImageView) view.findViewById(R.id.home_activity_user_pic);
+		if(basicDemographicInfo.getGender().equalsIgnoreCase("male")) {
+			imageView.setBackgroundResource(R.drawable.profile_male);
+		} else if(basicDemographicInfo.getGender().equalsIgnoreCase("female")) {
+			imageView.setBackgroundResource(R.drawable.profile_female);
+		} else {
+			imageView.setBackgroundResource(R.drawable.profile_unknown);
+		}
+
+		// set and display the user's full name
+		textView = (TextView) view.findViewById(R.id.home_activity_user_name);
+		fullName = person.getFirstName() + " " + person.getLastName();	
+		textView.setText(fullName);
+
+		// set and display the user's birthdate
+		textView = (TextView) view.findViewById(R.id.home_activity_user_birthdate);			
+		textView.setText(person.getBirthDate());
+
+		// set and display the user's gender
+		textView = (TextView) view.findViewById(R.id.home_activity_user_gender);
+		textView.setText(basicDemographicInfo.getGender());
+
+		// set and display the user's race
+		textView = (TextView) view.findViewById(R.id.home_activity_user_race);		
+		textView.setText(person.getRace());
+
+		// set and display the user's blood type
+		textView = (TextView) view.findViewById(R.id.home_activity_user_blood_type);			
+		textView.setText(person.getBloodType());
+
+		// set and display the user's height
+		textView = (TextView) view.findViewById(R.id.home_activity_user_height);		
+		textView.setText(height.getHeightValue() + " inches");
+
+		// set and display the user's weight
+		textView = (TextView) view.findViewById(R.id.home_activity_user_weight);
+		textView.setText(weight.getLbsValue() + " lbs");
+		
 	    return view;
 	}
 
